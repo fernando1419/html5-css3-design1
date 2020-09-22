a41180d0d672de52fecfa0c6ac32a2088020fb8f
Fernando Lamas <lamas.fernando1419@gmail.com>
2020-09-22 13:40:36 -0300

    Added owl carousel to display cards

--- js/main.js
+++ js/main.js
@@ -3,4 +3,23 @@ $(document).ready(function () {
    $('.toggle-collapse').click(function () {
       $nav.toggleClass('collapse');
    })
+
+   // Owl-carousel for blog
+   $('.owl-carousel').owlCarousel({
+      loop: true,
+      center: true,
+      autoplay: true,
+      // items: 1,
+      nav: true,
+      dots: true,
+      responsive: {
+         0: {
+            items: 1
+         },
+         600: {
+            items: 3
+         }
+      }
+   });
+
 });
