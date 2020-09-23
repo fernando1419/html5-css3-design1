3021c942705140a270e4b542ce0059eb3fc75ed8
Fernando Lamas <lamas.fernando1419@gmail.com>
2020-09-23 19:00:03 -0300

    Added footer section

--- js/main.js
+++ js/main.js
@@ -22,4 +22,11 @@ $(document).ready(function () {
       }
    });
 
+   // click to scroll top
+   $('.move-up span').click(function () {
+      $('html, body').animate({
+         scrollTop: 0
+      }, 2000)
+   });
+
 });
